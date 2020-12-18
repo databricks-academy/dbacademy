05d92916cb607fa1184d539f7d0bd002334c0808
Joshua Cook <joshua.cook@databricks.com>
2020-12-18 12:08:55 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # `dbtest`
+
+```
+conda create -n dbtest python=3.7
+conda activate dbtest
+pip install -r requirements.txt
+pytest
+```
---
+++ tests/test_dbtest.py
@@ -0,0 +1,9 @@
+import dbtest
+
+
+class TestDbtest:
+    def test_it_works(self):
+        assert True
+
+    def test_it_does_not_work(self):
+        assert not False
