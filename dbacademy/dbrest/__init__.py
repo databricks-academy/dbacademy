7e2ff1019b3b59f3c904b85bdd5a25cfd0b28298
Joshua Cook <joshua.cook@databricks.com>
2021-01-12 17:18:50 -0800

    add local and list dirs

--- dbacademy/dbrest/__init__.py
+++ dbacademy/dbrest/__init__.py
@@ -1,20 +1,38 @@
-from dbacademy import dbgems
+import configparser
+import os
 
 
 class DBAcademyRestClient:
+    def __init__(self, local=False, config_file=None, profile="DEFAULT"):
+        if not local:
+            from dbacademy import dbgems
 
-    def __init__(self, token=dbgems.get_notebooks_api_token(), endpoint=dbgems.get_notebooks_api_endpoint()):
-        self.token = token
-        self.endpoint = endpoint
+            self.token = dbgems.get_notebooks_api_token()
+            self.endpoint = dbgems.get_notebooks_api_endpoint()
+        else:
+            self._get_local_credentials(config_file, profile)
 
     def workspace(self):
         from dbacademy.dbrest.workspace import WorkspaceClient
+
         return WorkspaceClient(self, self.token, self.endpoint)
 
     def jobs(self):
         from dbacademy.dbrest.jobs import JobsClient
+
         return JobsClient(self, self.token, self.endpoint)
 
     def runs(self):
         from dbacademy.dbrest.runs import RunsClient
+
         return RunsClient(self, self.token, self.endpoint)
+
+    def _get_local_credentials(self, config_file, profile):
+        if config_file is None:
+            config_file = os.environ["HOME"] + "/.databrickscfg"
+        config = configparser.ConfigParser()
+        config.read(config_file)
+
+        self.endpoint = config.get(profile, "host")
+        self.username = config.get(profile, "username")
+        self.token = config.get(profile, "token")
--- dbacademy/dbrest/workspace.py
+++ dbacademy/dbrest/workspace.py
@@ -1,23 +1,34 @@
 import json
 import requests
 from dbacademy.dbtest import DBAcademyRestClient
+import pandas as pd
 
 
 class WorkspaceClient:
-
     def __init__(self, client: DBAcademyRestClient, token: str, endpoint: str):
         self.client = client
         self.token = token
         self.endpoint = endpoint
 
+    def ls(self, path):
+        auth_header = {"Authorization": "Bearer " + self.token + ""}
+        uri = f"{self.endpoint}/api/2.0/workspace/list?path={path}"
+        response = requests.get(uri, headers=auth_header)
+        assert response.status_code == 200, f"({response.status_code}): {response.text}"
+        return response.json()["objects"]
+
+    def ls_pd(self, path):
+        objects = pd.DataFrame(self.ls(path))
+        objects["object"] = objects["path"].apply(lambda path: path.split("/")[-1])
+        return_cols = ["object", "object_type", "object_id", "language", "path"]
+        return objects[return_cols].sort_values("object")
+
     def mkdirs(self, path) -> requests.Response:
-        payload = {
-            "path": path
-        }
+        payload = {"path": path}
         response = requests.post(
             f"{self.endpoint}/api/2.0/workspace/mkdirs",
             headers={"Authorization": "Bearer " + self.token},
-            data=json.dumps(payload)
+            data=json.dumps(payload),
         )
         assert response.status_code in [200], f"({response.status_code}): {response.text}"
         return response
@@ -25,32 +36,32 @@ class WorkspaceClient:
     def delete_path(self, path) -> requests.Response:
         print("-" * 80)
         print(f"Deleting {path}")
-        payload = {
-            "path": path,
-            "recursive": True
-        }
+        payload = {"path": path, "recursive": True}
         response = requests.post(
             f"{self.endpoint}/api/2.0/workspace/delete",
             headers={"Authorization": "Bearer " + self.token},
-            data=json.dumps(payload)
+            data=json.dumps(payload),
         )
-        assert response.status_code in [200, 404], f"({response.status_code}): {response.text}"
+        assert response.status_code in [
+            200,
+            404,
+        ], f"({response.status_code}): {response.text}"
         return response
 
     def import_notebook(self, language, notebook_path, content) -> requests.Response:
         import base64
 
         payload = {
-            "content": base64.b64encode(content.encode('utf-8')).decode("utf-8"),
+            "content": base64.b64encode(content.encode("utf-8")).decode("utf-8"),
             "path": notebook_path,
             "language": language,
             "overwrite": True,
-            "format": "SOURCE"
+            "format": "SOURCE",
         }
         response = requests.post(
             f"{self.endpoint}/api/2.0/workspace/import",
             headers={"Authorization": "Bearer " + self.token},
-            data=json.dumps(payload)
+            data=json.dumps(payload),
         )
         assert response.status_code in [200], f"({response.status_code}): {response.text}"
         return response
@@ -58,9 +69,6 @@ class WorkspaceClient:
     def export_notebook(self, notebook_path) -> str:
         auth_header = {"Authorization": "Bearer " + self.token + ""}
         uri = f"{self.endpoint}/api/2.0/workspace/export?path={notebook_path}&direct_download=true"
-        response = requests.get(
-            uri,
-            headers=auth_header
-        )
+        response = requests.get(uri, headers=auth_header)
         assert response.status_code == 200, f"({response.status_code}): {response.text}"
         return response.text
