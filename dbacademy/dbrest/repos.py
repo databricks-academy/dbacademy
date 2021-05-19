8f74abe94237bef9737625165af673b04d8af584
Jacob D. Parr <jacob.parr@databricks.com>
2021-05-19 16:29:18 -0500

    Added missing requirement for the name of the branch

--- dbacademy/dbrest/repos.py
+++ dbacademy/dbrest/repos.py
@@ -11,70 +11,13 @@ class ReposClient:
         self.token = token
         self.endpoint = endpoint
 
-    def update(self, repo_id):
+    def update(self, repo_id, branch):
         response = requests.patch(
             f"{self.endpoint}/api/2.0/repos/{repo_id}",
-            headers={"Authorization": "Bearer " + self.token}
+            headers={"Authorization": "Bearer " + self.token},
+            data=json.dumps({
+                "branch": branch
+            })
         )
         assert response.status_code == 200, f"({response.status_code}): {response.text}"
         return response.json()
-
-    def run_now(self, job_id):
-        response = requests.post(
-            f"{self.endpoint}/api/2.0/jobs/run-now",
-            headers={"Authorization": f"Bearer {self.token}"},
-            data=json.dumps({"job_id": job_id})
-        )
-        assert response.status_code == 200, f"({response.status_code}): {response.text}"
-        return response.json()
-
-    def delete_by_job_id(self, job_id):
-        response = requests.post(
-            f"{self.endpoint}/api/2.0/jobs/delete",
-            headers={"Authorization": f"Bearer {self.token}"},
-            data=json.dumps({"job_id": job_id})
-        )
-        assert response.status_code == 200, f"({response.status_code}): {response.text}"
-
-    def delete_by_name(self, jobs, success_only):
-        if type(jobs) == dict:
-            job_list = list(jobs.keys())
-        elif type(jobs) == list:
-            job_list = jobs
-        elif type(jobs) == str:
-            job_list = [jobs]
-        else:
-            raise Exception(f"Unsupported type: {type(jobs)}")
-
-        response = requests.get(
-            f"{self.endpoint}/api/2.0/jobs/list",
-            headers={"Authorization": f"Bearer {self.token}"}
-        )
-        assert response.status_code == 200, f"({response.status_code}): {response.text}"
-
-        deleted = 0
-        jobs = response.json()["jobs"]
-        print(f"Found {len(jobs)} jobs total")
-
-        for job_name in job_list:
-            for job in jobs:
-                if job_name == job["settings"]["name"]:
-                    job_id = job["job_id"]
-
-                    runs = self.client.runs().list_by_job_id(job_id)
-                    delete_job = True
-
-                    for run in runs:
-                        state = run["state"]
-                        if success_only and state["life_cycle_state"] != "TERMINATED":
-                            delete_job = False
-                            print(f"""The job "{job_name}" was not "TERMINATED" but "{state["life_cycle_state"]}", this job must be deleted manually""")
-                        if success_only and state["result_state"] != "SUCCESS":
-                            delete_job = False
-                            print(f"""The job "{job_name}" was not "SUCCESS" but "{state["result_state"]}", this job must be deleted manually""")
-
-                    if delete_job:
-                        self.delete_by_job_id(job_id)
-                        deleted += 1
-
-        print(f"Deleted {deleted} jobs")
