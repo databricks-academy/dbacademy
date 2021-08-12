356a02814154212a1d018b3058c835a7abf694b8
Jacob D. Parr <me@jacobparr.com>
2021-08-12 16:43:15 -0500

    x

--- dbacademy/dbpublish/__init__.py
+++ dbacademy/dbpublish/__init__.py
@@ -246,7 +246,7 @@ def publish(source_project:str, target_project:str, notebook_name:str, replaceme
         for token in bdc_tokens:
             assert token not in command, f"Found {token} in command #{i+1}"
 
-        print(("-"*80) + " " + ("INSTRUCTOR_NOTE" in bdc_tokens))
+        print(("-"*80) + " " + str("INSTRUCTOR_NOTE" in bdc_tokens))
         print(command)
            
     assert found_header_directive, f"One of the two header directives ({D_INCLUDE_HEADER_TRUE} or {D_INCLUDE_HEADER_FALSE}) were not found."
