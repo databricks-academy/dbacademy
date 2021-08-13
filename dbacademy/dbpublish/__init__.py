91e92e581fc6b43398ba8a4c7db3d4bc211108f8
Jacob D. Parr <me@jacobparr.com>
2021-08-12 20:44:15 -0500

    x

--- dbacademy/dbpublish/__init__.py
+++ dbacademy/dbpublish/__init__.py
@@ -137,7 +137,7 @@ def clean_todo_cell(command, cmd):
     for i in range(len(lines)):
         line = lines[i]
         if i==0 and line.strip().replace(" ", "") not in ["#TODO", "##TODO"]:
-            raise Exception(f"""Expected line #1 in Cmd #{cmd+1} to be the {D_TODO} directive: "{line}" """)
+            raise Exception(f"""Expected line #1 in Cmd #{cmd+1} to be the {D_TODO} directive: "{line}"\n{command}""")
         elif not line.startswith("#") and line.strip() != "":
             raise Exception(f"""Expected line #{i+1} in Cmd #{cmd+1} to be commented out: "{line}" """)
         elif i==0 or line.strip() == "":
