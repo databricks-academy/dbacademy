c026fdf63a835039b3c7a5c594a7d10a4db18d5b
Jacob D. Parr <me@jacobparr.com>
2021-08-20 12:55:30 -0500

    x

--- dbacademy/dbrest/workspace.py
+++ dbacademy/dbrest/workspace.py
@@ -49,7 +49,7 @@ class WorkspaceClient:
         return self.client.execute_get(f"{self.endpoint}/api/2.0/workspace/export?path={notebook_path}&direct_download=true").text
 
     def get_status(self, notebook_path) -> dict:
-        response = self.client.execute_get(f"{self.endpoint}/api/2.0/workspace/get-status?path={notebook_path}", expected=[200,404]))
+        response = self.client.execute_get(f"{self.endpoint}/api/2.0/workspace/get-status?path={notebook_path}", expected=[200,404])
         if response.status_code == 404:
             return None
         else:
