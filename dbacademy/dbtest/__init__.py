8a4e2d79708449d294f62ba9c0e653d171c771ce
Jacob D. Parr <me@jacobparr.com>
2021-08-31 11:10:29 -0500

    x

--- dbacademy/dbtest/__init__.py
+++ dbacademy/dbtest/__init__.py
@@ -249,7 +249,7 @@ class SuiteBuilder:
         if self.client.workspace().get_status(notebook_path) is None:
           raise Exception(f"Notebook not found: {notebook_path}")
 
-        hash = hashlib.sha256(notebook_path.encode())
+        hash = hashlib.sha256(notebook_path.encode()).hexdigest()
         job_name = f"[TEST] {self.course_name} | {self.test_type} | {hash}"
         self.jobs[job_name] = (notebook_path, 0, 0, ignored)
 
