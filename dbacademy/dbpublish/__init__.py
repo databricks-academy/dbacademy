a97373e5a4807e26fcf8ea2da8b841645a6f52ba
Jacob D. Parr <me@jacobparr.com>
2021-11-07 15:01:49 -0600

    x

--- dbacademy/dbpublish/__init__.py
+++ dbacademy/dbpublish/__init__.py
@@ -293,6 +293,8 @@ def clean_todo_cell(source_language, command, cmd):
         prefix = f"{source_m} MAGIC {cell_m}"
         print(f"{source_m} vs {prefix}")
 
+    print("Prefix: {prefix}")
+
     for i in range(len(lines)):
         line = lines[i]
 
