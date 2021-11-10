b9ddfd5a7e86282d7abd56c96bdc4ae648e0487d
Jacob D. Parr <me@jacobparr.com>
2021-11-10 14:15:00 -0600

    x

--- dbacademy/dbtest/__init__.py
+++ dbacademy/dbtest/__init__.py
@@ -159,7 +159,7 @@ class TestConfig:
         else:
           print(f"self.notebooks:")
           for notebook in self.notebooks:
-            print(notebook)
+            print(f"  {notebook}")
 
 
 def create_test_job(client, test_config, job_name, notebook_path):
