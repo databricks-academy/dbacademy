93c532bb9ecfc71ab26426b008176de98993f662
Jacob D. Parr <me@jacobparr.com>
2021-11-10 18:41:19 -0600

    x

--- dbacademy/dbtest/__init__.py
+++ dbacademy/dbtest/__init__.py
@@ -351,7 +351,7 @@ class TestSuite:
           if round > 0:
             hash = hashlib.sha256(notebook_path.encode()).hexdigest()
             job_name = f"[TEST] {test_config.name} | {test_type} | {hash}"
-            rounds[round][job_name] = (notebook_path, 0, 0, ignored)
+            self.rounds[round][job_name] = (notebook_path, 0, 0, ignored)
 
             if self.client.workspace().get_status(notebook_path) is None:
               raise Exception(f"Notebook not found: {notebook_path}")
