6d3327b17168f9bcd593b640420287c6d27b5015
Jacob D. Parr <me@jacobparr.com>
2022-01-14 17:18:19 -0600

    x

--- dbacademy/dbrest/permissions.py
+++ dbacademy/dbrest/permissions.py
@@ -7,6 +7,9 @@ class PermissionsClient:
         self.token = token        # The authentication token
         self.endpoint = endpoint  # The API endpoint
 
+    def get_job_permissions(self, job_id):
+        return client.execute_get_json(f"{client.endpoint}/api/2.0/permissions/jobs/{job_id}/permissionLevels")
+
     def change_job_owner(self, job_id, username):
         params = {
             "access_control_list": [
