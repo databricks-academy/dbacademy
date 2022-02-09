95183b313b76592bd6c476132763dc349948e85c
Jacob D. Parr <me@jacobparr.com>
2022-02-09 17:23:07 -0600

    x

--- dbacademy/dbpublish/publisher_class.py
+++ dbacademy/dbpublish/publisher_class.py
@@ -23,6 +23,7 @@ class Publisher:
 
     def add_notebook(self, notebook):
         from datetime import datetime
+        from dbacademy.dbtest import NotebookDef
 
         assert type(notebook) == NotebookDef, f"""Expected the parameter "notebook" to be of type "NotebookDef", found "{type(notebook)}" """
 
