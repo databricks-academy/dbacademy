47719c1a338c7343bd1f3ce478d550963979361e
Jacob D. Parr <me@jacobparr.com>
2022-03-02 16:49:35 -0600

    x

--- dbacademy/dbrest/scim/users.py
+++ dbacademy/dbrest/scim/users.py
@@ -8,7 +8,11 @@ class ScimUsersClient:
         self.endpoint = endpoint  # The API endpoint
 
     def list(self):
-        return self.client.execute_get_json(f"{self.endpoint}/api/2.0/preview/scim/v2/Users")
+        response = self.client.execute_get_json(f"{self.endpoint}/api/2.0/preview/scim/v2/Users")
+        users = response.get("Resources", list())
+        totalResults = response.get("totalResults")
+        assert len(users) == int(totalResults), f"The totalResults ({totalResults}) does not match the number of records ({len(users)}) returned"
+        return users
 
     def get(self, user_id):
         url = f"{self.endpoint}/api/2.0/preview/scim/v2/Users/{user_id}"
