c935f38f34684c11edb66e01de204f08d29ff4ec
Jacob D. Parr <me@jacobparr.com>
2022-03-02 20:00:06 -0600

    x

--- dbacademy/dbrest/sql/endpoints.py
+++ dbacademy/dbrest/sql/endpoints.py
@@ -217,16 +217,12 @@ class SqlEndpointsClient:
         print(f"Creating the endpoint \"{endpoint_name}\" for the user \"{username}\"")
         print("-"*80)    
         
-        self.create(self, endpoint_name,
-                          cluster_size,
-                          enable_serverless_compute)
-
-                        #   cluster_size)
-                        #   enable_serverless_compute=enable_serverless_compute,
-                        #   min_num_clusters=min_num_clusters,
-                        #   max_num_clusters=max_num_clusters,
-                        #   auto_stop_mins=auto_stop_mins,
-                        #   enable_photon=enable_photon,
-                        #   spot_instance_policy=spot_instance_policy,
-                        #   channel=channel,
-                        #   tags=tags)
+        self.create(self, name=endpoint_name,
+                          enable_serverless_compute=enable_serverless_compute,
+                          min_num_clusters=min_num_clusters,
+                          max_num_clusters=max_num_clusters,
+                          auto_stop_mins=auto_stop_mins,
+                          enable_photon=enable_photon,
+                          spot_instance_policy=spot_instance_policy,
+                          channel=channel,
+                          tags=tags)
