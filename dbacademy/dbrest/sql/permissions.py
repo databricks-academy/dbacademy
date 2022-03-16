78295da1aae0d3a72ea5dcbd97ccf8af018279d4
Jacob D. Parr <me@jacobparr.com>
2022-03-16 15:22:15 -0500

    x

--- dbacademy/dbrest/sql/permissions.py
+++ dbacademy/dbrest/sql/permissions.py
@@ -10,9 +10,37 @@ class SqlPermissionsClient:
         self.base_uri = f"{self.endpoint}/api/2.0/preview/sql/permissions"
 
         self.max_page_size = 250
+        self.valid_objects = ["alerts", "dashboards", "data_sources", "queries"]
+        self.valid_permissions = ["CAN_VIEW", "CAN_RUN", "CAN_MANAGE"]
+
+    def _validate_object_type(object_type):
+        assert object_type in self.valid_objects, f"Expected \"object_type\" to be one of {self.valid_objects}, found \"{object_type}\""
+
+    def _validate_permission_level(permission_level):
+        assert permission_level in self.valid_permissions, f"Expected \"permission_level\" to be one of {self.valid_permissions}, found \"{permission_level}\""
 
     def get(self, object_type, object_id):
-        objects = ["alerts", "dashboards", "data_sources", "queries"]
-        assert object_type in objects, f"Expected object type to be one of {objects}, found {object_type}"
-
+        self._validate_object_type(object_type)
         return self.client.execute_get_json(f"{self.base_uri}/{object_type}/{object_id}")
+
+    def update_group(self, object_type, object_id, group_name, permission_level):
+        self._validate_object_type(object_type)
+        self._validate_permission_level(permission_level)
+
+        params = {
+            "group_name": group_name,
+            "permission_level": permission_level
+        }
+
+        return self.client.execute_post_json(f"{self.base_uri}/{object_type}/{object_id}", params)
+
+    def update_group(self, object_type, object_id, user_name, permission_level):
+        self._validate_object_type(object_type)
+        self._validate_permission_level(permission_level)
+
+        params = {
+            "user_name": user_name,
+            "permission_level": permission_level
+        }
+
+        return self.client.execute_post_json(f"{self.base_uri}/{object_type}/{object_id}", params)
