981f74474df78f5bca5cb5cce1d919e8ce9344e3
Jacob D. Parr <me@jacobparr.com>
2022-03-16 12:04:02 -0500

    x

--- dbacademy/dbrest/sql/queries.py
+++ dbacademy/dbrest/sql/queries.py
@@ -1,4 +1,5 @@
 from dbacademy.dbrest import DBAcademyRestClient
+import builtins
 
 class SqlQueriesClient:
 
@@ -8,8 +9,13 @@ class SqlQueriesClient:
         self.endpoint = endpoint
         self.base_uri = f"{self.endpoint}/api/2.0/preview/sql/queries"
 
-    def list(self):
-        return self.client.execute_get_json(f"{self.base_uri}")
+    def list(self, queries=builtins.list(), page=1):
+        json_response = self.client.execute_get_json(f"{self.base_uri}?page_size=1000&page={len(queries)+1}")
+
+        queries.extend(json_response.get("results", builtins.list()))
+
+        if json_response.get("count") == len(queries): return queries
+        else: return self.list(queries, page+1)
 
     def get_by_id(self, query_id):
         return self.client.execute_get_json(f"{self.base_uri}/{query_id}")
