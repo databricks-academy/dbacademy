43e85b7049ffc5ce1c054c7ca34c86a2ee2374ff
Jacob D. Parr <me@jacobparr.com>
2022-03-16 12:33:55 -0500

    x

--- dbacademy/dbrest/sql/queries.py
+++ dbacademy/dbrest/sql/queries.py
@@ -9,10 +9,12 @@ class SqlQueriesClient:
         self.endpoint = endpoint
         self.base_uri = f"{self.endpoint}/api/2.0/preview/sql/queries"
 
+        self.max_page_size = 250
+
     def list(self, queries=None, page=1):
         if queries is None: queries = builtins.list()
-        max_page_size = 250
-        url = f"{self.base_uri}?page_size={max_page_size}&page={page}"
+
+        url = f"{self.base_uri}?page_size={self.max_page_size}&page={page}"
         json_response = self.client.execute_get_json(url)
 
         queries.extend(json_response.get("results", builtins.list()))
@@ -22,3 +24,19 @@ class SqlQueriesClient:
 
     def get_by_id(self, query_id):
         return self.client.execute_get_json(f"{self.base_uri}/{query_id}")
+
+    def get_by_name(self, query_name, queries=None, page=1):
+        if queries is None: queries = builtins.list()
+
+        url = f"{self.base_uri}?page_size={self.max_page_size}&page={page}"
+        json_response = self.client.execute_get_json(url)
+
+        queries.extend(json_response.get("results", builtins.list()))
+
+        for query in queries:
+            if query_name == query.get("name"):
+                return query
+
+        # Not found, continue looking.
+        if json_response.get("count") == len(queries): return queries
+        else: return self.get_by_name(query_name=query_name, queries=queries, page=page+1)
