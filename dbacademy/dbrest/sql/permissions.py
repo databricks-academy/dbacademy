14296a69f89e13c5acde88324c337d27f3a39b13
Jacob D. Parr <me@jacobparr.com>
2022-03-16 15:26:35 -0500

    x

--- dbacademy/dbrest/sql/permissions.py
+++ dbacademy/dbrest/sql/permissions.py
@@ -13,14 +13,14 @@ class SqlPermissionsClient:
         self.valid_objects = ["alerts", "dashboards", "data_sources", "queries"]
         self.valid_permissions = ["CAN_VIEW", "CAN_RUN", "CAN_MANAGE"]
 
-    def _validate_object_type(object_type):
+    def _validate_object_type(self, object_type):
         assert object_type in self.valid_objects, f"Expected \"object_type\" to be one of {self.valid_objects}, found \"{object_type}\""
 
     def _validate_permission_level(permission_level):
         assert permission_level in self.valid_permissions, f"Expected \"permission_level\" to be one of {self.valid_permissions}, found \"{permission_level}\""
 
     def get(self, object_type, object_id):
-        self._validate_object_type(object_type)
+        self._validate_object_type(self, object_type)
         return self.client.execute_get_json(f"{self.base_uri}/{object_type}/{object_id}")
 
     def update_group(self, object_type, object_id, group_name, permission_level):
