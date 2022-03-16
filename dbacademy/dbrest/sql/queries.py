58c0fb02f78588f54872ca763dde8e1ff9d1809c
Jacob D. Parr <me@jacobparr.com>
2022-03-16 12:20:56 -0500

    x

--- dbacademy/dbrest/sql/queries.py
+++ dbacademy/dbrest/sql/queries.py
@@ -11,8 +11,9 @@ class SqlQueriesClient:
 
     def list(self, queries=builtins.list(), page=1):
         max_page_size = 250
-        print(f"Fetching page {page}")
-        json_response = self.client.execute_get_json(f"{self.base_uri}?page_size={max_page_size}&page={len(queries)+1}")
+        url = f"{self.base_uri}?page_size={max_page_size}&page={len(queries)+1}"
+        print(f"Fetching: {self.base_uri}")
+        json_response = self.client.execute_get_json(url)
 
         queries.extend(json_response.get("results", builtins.list()))
 
