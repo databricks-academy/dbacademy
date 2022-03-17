4214fe8a090c1620c8b9e772177b908d5081d057
Jacob D. Parr <me@jacobparr.com>
2022-03-16 20:11:08 -0500

    x

--- dbacademy/dbrest/permissions/__init__.py
+++ dbacademy/dbrest/permissions/__init__.py
@@ -22,6 +22,10 @@ class SqlProxy():
         from dbacademy.dbrest.permissions.sql_permissions_client import SqlPermissionsClient
         return SqlPermissionsClient(self.client, self.token, self.endpoint, "data_source", "data_sources")
 
+    def alert(self):
+        from dbacademy.dbrest.permissions.sql_permissions_client import SqlPermissionsClient
+        return SqlPermissionsClient(self.client, self.token, self.endpoint, "alert", "alerts")
+
 class PermissionsClient():
     def __init__(self, client: DBAcademyRestClient, token: str, endpoint: str):
         self.client = client      # Client API exposing other operations to this class
