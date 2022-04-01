5303be90464eebf2e8fdb4ac2bf864618e7546b9
Jacob D. Parr <me@jacobparr.com>
2022-04-01 10:47:06 -0500

    x

--- dbacademy/dbgems/__init__.py
+++ dbacademy/dbgems/__init__.py
@@ -4,7 +4,11 @@ from pyspark import SparkContext
 from pyspark.sql import SparkSession
 
 print("="*80)
-print("DEPRECATION WARNING: This API has been deprecated.\nPlease use https://github.com/databricks-academy/dbacademy-gems instead.\nIf you see this message or if you have any questions, please reach out to Jacob Parr via Slack.")
+print("""
+DEPRECATION WARNING: The DBGems API found in https://github.com/databricks-academy has been deprecated.
+Please use https://github.com/databricks-academy/dbacademy-gems instead.
+If you see this message or if you have any questions, please reach out to Jacob Parr via Slack.
+""".strip())
 print("="*80)
 
 def init_locals():
