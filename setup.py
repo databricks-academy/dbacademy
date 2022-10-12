029fa59cdb38dbcb37bef14e2c05ab181c44927c
SireInsectus <jacob.parr@databricks.com>
2022-10-12 11:58:41 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 import setuptools
 from setuptools import find_packages
 
+reqs = [
+    "setuptools",
+    "requests",
+    "urllib3",
+    "overrides",
+    "Deprecated",
+]
+
 setuptools.setup(
     name="dbacademy",
     author="Databricks, Inc",
     maintainer="Databricks Academy",
     version="v0.0.0",
-    package_dir={"": "src"},
+    install_requires=reqs,
     packages=find_packages(where="src")
 )
