15d27833d5977c95361cbc7bb95a9d5d267a12ff
SireInsectus <jacob.parr@databricks.com>
2022-10-17 09:13:23 -0500

    Fixed error message

--- src/dbacademy/dbgems/__init__.py
+++ src/dbacademy/dbgems/__init__.py
@@ -204,7 +204,7 @@ def validate_dependencies(module: str, curriculum_workspaces_only=True) -> bool:
                     return True  # They match, all done!
 
                 print_warning(title=f"Outdated Dependency",
-                              message=f"You are using version \"v{current_version}\" but the latest version is \"v{versions[-1]}\".\n" +
+                              message=f"You are using version \"{current_version}\" but the latest version is \"v{versions[-1]}\".\n" +
                                       f"Please update your dependencies on the module \"{module}\" at your earliest convenience.")
             else:
                 print_warning(title=f"Invalid Dependency",
