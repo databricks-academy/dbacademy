bb8da94f8d396a851d2d3f615640753d60a1ca05
SireInsectus <jacob.parr@databricks.com>
2022-10-28 16:00:08 -0500

    Update translator_class.py

--- src/dbacademy/dbbuild/publish/translator_class.py
+++ src/dbacademy/dbbuild/publish/translator_class.py
@@ -104,8 +104,9 @@ class Translator:
     def create_published_message(self):
 
         change_log = self.build_config.change_log
-        # if self.build_config.change_log is None:
-        #     # Expected to be None for translations.
+        if self.build_config.change_log is None:
+            change_log = BuildUtils.load_change_log(source_repo=self.source_repo,
+                                                    target_version=self.core_version)
 
         return BuildUtils.create_published_message(source_repo=self.source_repo,
                                                    name=self.build_config.name,
