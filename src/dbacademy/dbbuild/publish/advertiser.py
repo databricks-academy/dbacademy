e4110c7f3d80dbdc2bede7517956be4eb0c58423
SireInsectus <jacob.parr@databricks.com>
2022-10-29 20:08:57 -0500

    Update advertiser.py

--- src/dbacademy/dbbuild/publish/advertiser.py
+++ src/dbacademy/dbbuild/publish/advertiser.py
@@ -3,8 +3,6 @@ class Advertiser:
     from dbacademy.dbbuild.publish.publishing_info_class import PublishingInfo
 
     def __init__(self, *, name: str, version: str, change_log: ChangeLog, publishing_info: PublishingInfo, source_repo: str, common_language):
-        import urllib.parse
-
         self.__name = name
         self.__version = version
         self.__change_log = change_log
@@ -15,11 +13,6 @@ class Advertiser:
         self.__create_message()
 
         self.__subject = f"Published {self.__name}, v{self.__version}"
-
-        self.__email_subject = urllib.parse.quote(self.__subject, safe="")
-        self.__email_body = urllib.parse.quote(self.__message, safe="")
-
-        self.__slack_message = f"@channel {self.__subject}\n\n{self.__message.strip()}"
 
         self.__create_html()
 
@@ -33,13 +26,18 @@ class Advertiser:
         self.__message += f"""
 Release notes, course-specific requirements, issue-tracking, and test results for this course can be found in the course's GitHub repository at https://github.com/databricks-academy/{self.__source_repo.split("/")[-1]}
 
-Please contact me (via Slack), or anyone on the curriculum team should you have any questions."""
+Please contact me (via Slack), or anyone on the curriculum team should you have any questions.""".strip()
 
     def __create_html(self):
+        import urllib.parse
+
+        email_subject = urllib.parse.quote(self.__subject, safe="")
+        email_body = urllib.parse.quote(self.__message, safe="")
+
         content = """<div style="margin-bottom:1em">"""
 
         for address in self.__publishing_info.announcements.email_addresses:
-            url = f"mailto:{address}?subject={self.__email_subject}&body={self.__email_body}"
+            url = f"mailto:{address}?subject={email_subject}&body={email_body}"
             content += f"""<li><a href="{url}" target="_blank">{address}</a></li>"""
 
         for channel in self.__publishing_info.announcements.slack_channels:
@@ -47,9 +45,10 @@ Please contact me (via Slack), or anyone on the curriculum team should you have 
 
         content += "</div>"
 
-        rows = len(self.__slack_message.split("\n")) + 1
+        rows = len(self.__message.split("\n")) + 1
         self.__html = f"""
         <body style="font-size:16px">
             {content}
-            <textarea style="width:100%; padding:1em" rows={rows}>{self.__slack_message}</textarea>
+            <div><input style="width:100%" type="text" value="{self.__subject}"></div>
+            <textarea style="width:100%; padding:1em" rows={rows}>{self.__message}</textarea>
         </body>"""
