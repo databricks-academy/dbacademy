c49c42026e78a767e7dadba3c9ece67989f15333
SireInsectus <jacob.parr@databricks.com>
2022-11-02 14:31:25 -0500

    Update clusters.py

--- src/dbacademy/dbrest/clusters.py
+++ src/dbacademy/dbrest/clusters.py
@@ -1,6 +1,6 @@
 from dbacademy.dbrest import DBAcademyRestClient
 from dbacademy.rest.common import ApiContainer
-
+from dbacademy import common
 
 class ClustersClient(ApiContainer):
     def __init__(self, client: DBAcademyRestClient):
@@ -10,11 +10,13 @@ class ClustersClient(ApiContainer):
         return self.client.api("GET", f"{self.client.endpoint}/api/2.0/clusters/list")
 
     def get(self, cluster_id):
+        cluster_id = common.validate_type(cluster_id, "cluster_id", str)
+
         return self.client.api("GET", f"{self.client.endpoint}/api/2.0/clusters/get?cluster_id={cluster_id}")
 
     def get_current_spark_version(self):
         from dbacademy import dbgems
-        cluster_id = dbgems.get_tags()["clusterId"]
+        cluster_id = dbgems.get_tags().get("clusterId")
         return self.get(cluster_id).get("spark_version", None)
 
     def get_current_instance_pool_id(self):
