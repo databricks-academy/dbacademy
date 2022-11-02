858436c088dd99f7d5af9451af8678283945fa93
SireInsectus <jacob.parr@databricks.com>
2022-11-02 14:30:40 -0500

    Updated mock objects

--- src/dbacademy/dbgems/mock_dbutils_class.py
+++ src/dbacademy/dbgems/mock_dbutils_class.py
@@ -1,20 +1,73 @@
-class Stub:
+class MockOptional:
+    def __init__(self, value: str):
+        self.value = value
 
     # noinspection PyPep8Naming
-    def getDbutils(self):
-        return self
+    def getOrElse(self, default_value: str):
+        return self.value or default_value
+
+
+class MockFileSystem:
+    def __init__(self):
+        pass
+
+
+class MockEntryPoint:
+    # noinspection PyPep8Naming
+    @staticmethod
+    def getDbutils() -> "MockDBUtils":
+        # tags = dbutils.entry_point.getDbutils().notebook().getContext().tags()
+        return MockDBUtils()
+
+
+class MockWidgets:
+    def __init__(self):
+        pass
+
+
+class MockNotebook:
+    # tags = dbutils.entry_point.getDbutils().notebook().getContext().tags()
+    # noinspection PyPep8Naming
+    @staticmethod
+    def getContext() -> "MockContext":
+        return MockContext()
+
+
+class MockContext:
+    @staticmethod
+    def tags():
+        return {
+            "orgId": "mock-00",
+            "clusterId": "mock-0",
+        }
+
+    # noinspection PyPep8Naming
+    @staticmethod
+    def notebookPath() -> MockOptional:
+        # dbutils.entry_point.getDbutils().notebook().getContext().notebookPath().getOrElse(None)
+        path = "/Repos/Temp/mock-course-source/Source/Version Info"
+        return MockOptional(path)
+
+
+class MockSecrets:
+    def __init__(self):
+        pass
 
 
 class MockDBUtils:
     def __init__(self):
-        self.fs = None
-        self.widgets = None
-        self.notebook = None
-        self.secrets = None
-        self.entry_point = Stub()
+        self.fs = MockFileSystem()
+        self.widgets = MockWidgets()
+        self.secrets = MockSecrets()
+        self.entry_point = MockEntryPoint()
 
     # noinspection PyPep8Naming
-    def displayHTML(self, **kwargs): pass
+    def displayHTML(self, **kwargs):
+        pass
 
-    # noinspection PyPep8Naming
-    def display(self, **kwargs): pass
+    def display(self, **kwargs):
+        pass
+
+    @staticmethod
+    def notebook() -> MockNotebook:
+        return MockNotebook()
