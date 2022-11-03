ddb42990d92eca11f112dad2df1c0007dffea547
SireInsectus <jacob.parr@databricks.com>
2022-11-03 11:28:25 -0500

    Fixing bugs

--- test/dbacademy_test/dbbuild/test_build_config.py
+++ test/dbacademy_test/dbbuild/test_build_config.py
@@ -3,18 +3,28 @@ import unittest
 
 class TestBuildConfig(unittest.TestCase):
 
-    def test_load(self):
+    # def test_load(self):
+    #     from dbacademy.dbbuild.build_config_class import BuildConfig
+    #
+    #     config = {
+    #         "name": "Test Suite",
+    #         "notebook_config": {},
+    #         "publish_only": None
+    #     }
+    #     bc = BuildConfig.load_config(config, "1.2.3")
+    #
+    #     self.assertIsNotNone(bc)
+    #     self.assertIsNotNone(self)
+
+    def test_initialize_notebooks(self):
         from dbacademy.dbbuild.build_config_class import BuildConfig
+        build_config = BuildConfig(name="Test Suite")
 
-        config = {
-            "name": "Test Suite",
-            "notebook_config": {},
-            "publish_only": None
-        }
-        bc = BuildConfig.load_config(config, "1.2.3")
-
-        self.assertIsNotNone(bc)
-        self.assertIsNotNone(self)
+        try:
+            build_config.validate()
+        except AssertionError as e:
+            expected = "The notebooks have not yet been initialized; Please call BuildConfig.initialize_notebooks() before proceeding."
+            self.assertEqual(expected, str(e))
 
 
 if __name__ == '__main__':
