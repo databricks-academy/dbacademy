11aa2457c7594d969055cf90747c22d9881cd295
SireInsectus <jacob.parr@databricks.com>
2022-11-06 11:53:18 -0600

    Disabled parts of flaky unit tests

--- test/dbacademy_test/github/test_github_client_class.py
+++ test/dbacademy_test/github/test_github_client_class.py
@@ -15,8 +15,10 @@ class MyTestCase(unittest.TestCase):
 
         all_tags = github.default_client.repo("dbacademy").list_all_tags()
         self.assertTrue(len(all_tags) >= 30, f"Expected at least 30 entries, found {len(all_tags)}")
-        self.assertEqual("1.0.18", all_tags[0], f"Expected version #1 to be 1.0.12, found {all_tags[0]}")
-        self.assertEqual("2.0.1", all_tags[29], f"Expected version #1 to be 1.0.41, found {all_tags[29]}")
+
+        # This call is only returning the last 30 making the test flaky to the extent of validating any specific entries.
+        # self.assertEqual("1.0.20", all_tags[0], f"Expected version #1 to be 1.0.12, found {all_tags[0]}")
+        # self.assertEqual("2.0.1", all_tags[29], f"Expected version #1 to be 1.0.41, found {all_tags[29]}")
 
 
 if __name__ == '__main__':
