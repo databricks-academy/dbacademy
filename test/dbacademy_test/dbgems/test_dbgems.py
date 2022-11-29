c976c4efffc6c2c23deebcdb875afa7c92fcbe1b
SireInsectus <jacob.parr@databricks.com>
2022-11-28 18:38:32 -0600

    Added support for mocking out spark config and other values

---
+++ test/dbacademy_test/dbgems/test_dbgems.py
@@ -0,0 +1,20 @@
+import unittest
+from dbacademy import dbgems
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_spark_conf_get(self):
+        value = dbgems.get_spark_config("some.random.value")
+        self.assertIsNone(value)
+
+        value = dbgems.get_spark_config("some.random.value", "whatever")
+        self.assertEquals("whatever", value)
+
+        dbgems.MOCK_CONFIG["some.random.value"] = "apples"
+        value = dbgems.get_spark_config("some.random.value")
+        self.assertEquals("apples", value)
+
+
+if __name__ == '__main__':
+    unittest.main()
