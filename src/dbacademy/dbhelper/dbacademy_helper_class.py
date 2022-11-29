06d2c4ab647e7a704943a02b7bc90d32ce24dbaa
SireInsectus <jacob.parr@databricks.com>
2022-11-29 13:20:19 -0600

    Update dbacademy_helper_class.py

--- src/dbacademy/dbhelper/dbacademy_helper_class.py
+++ src/dbacademy/dbhelper/dbacademy_helper_class.py
@@ -617,20 +617,22 @@ class DBAcademyHelper:
                 fs.drop_table(name)
 
     def __cleanup_mlflow_models(self) -> None:
-        pass
-        # import mlflow
-        #
-        # # noinspection PyCallingNonCallable
-        # for rm in mlflow.models.utils. .list_registered_models(max_results=1000):
-        #     if rm.name.startswith(self.unique_name):
-        #         print(f"Deleting registered model {rm.name}")
-        #         for mv in rm.latest_versions:
-        #             if mv.current_stage in ["Staging", "Production"]:
-        #                 # noinspection PyUnresolvedReferences
-        #                 mlflow.transition_model_version_stage(name=rm.name, version=mv.version, stage="Archived")
-        #
-        #         # noinspection PyUnresolvedReferences
-        #         mlflow.delete_registered_model(rm.name)
+        import mlflow
+        from mlflow.entities import ViewType
+
+        # noinspection PyCallingNonCallable
+        for rm in mlflow.search_experiments(view_type=ViewType.ACTIVE_ONLY):
+            if not rm.name.startswith(self.unique_name):
+                print(f"Skipping registered model {rm.name}")
+            else:
+                print(f"Deleting registered model {rm.name}")
+                # for mv in rm.:
+                #     if mv.current_stage in ["Staging", "Production"]:
+                #         # noinspection PyUnresolvedReferences
+                #         mlflow.transition_model_version_stage(name=rm.name, version=mv.version, stage="Archived")
+
+                # noinspection PyUnresolvedReferences
+                mlflow.delete_registered_model(rm.name)
 
     @staticmethod
     def __cleanup_experiments() -> None:
