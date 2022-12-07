9d3ff605b63385cf9eac41a6b64471a58c02b697
SireInsectus <jacob.parr@databricks.com>
2022-12-07 16:01:23 -0600

    Publishing v3.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from setuptools import find_packages
 
 setuptools.setup(
-    version="v3.0.1",
+    version="v3.0.2",
     name="dbacademy",
     author="Databricks, Inc",
     maintainer="Databricks Academy",
