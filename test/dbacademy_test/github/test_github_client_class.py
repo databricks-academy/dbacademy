a7885fd5d68ffb40b34051131cdfaf68f8882e20
SireInsectus <jacob.parr@databricks.com>
2023-01-05 18:25:54 -0600

    Added Docebo client

--- test/dbacademy_test/github/test_github_client_class.py
+++ test/dbacademy_test/github/test_github_client_class.py
@@ -6,14 +6,14 @@ class MyTestCase(unittest.TestCase):
     def test_get_latest_commit_id(self):
         from dbacademy import github
 
-        commit_id = github.default_client.repo("dbacademy").commits.get_latest_commit_id(branch_name="main")
+        commit_id = github.default_client().repo("dbacademy").commits.get_latest_commit_id(branch_name="main")
 
         self.assertIsNotNone(commit_id)
 
     def test_list_all_tags(self):
         from dbacademy import github
 
-        all_tags = github.default_client.repo("dbacademy").list_all_tags()
+        all_tags = github.default_client().repo("dbacademy").list_all_tags()
         self.assertTrue(len(all_tags) >= 30, f"Expected at least 30 entries, found {len(all_tags)}")
 
         # This call is only returning the last 30 making the test flaky to the extent of validating any specific entries.
