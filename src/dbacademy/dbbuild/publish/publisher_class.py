f01321790587221104989c783605de28255dabe0
Jacob Parr <jacob.parr@corp.databricks.com>
2023-02-03 14:08:27 -0600

    Update publisher_class.py

--- src/dbacademy/dbbuild/publish/publisher_class.py
+++ src/dbacademy/dbbuild/publish/publisher_class.py
@@ -572,7 +572,9 @@ class Publisher:
             self.__changes_in_target_repo = 0
 
         elif self.target_repo_url is None:
-            common.print_warning(f"SKIPPING VALIDATION", "This course is not being published to a GitHub repo")
+            msg = "Aborting build to force manual-confirmation before publishing."
+            common.print_warning(f"SKIPPING VALIDATION", f"This course is not being published to a GitHub repo.\n{msg}")
+            raise Exception(msg)
 
         else:
             repo_name = f"{self.build_name}.git"
