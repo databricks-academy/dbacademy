89d39bfc42fad8c71345bd4be4f2f8aee30ddd89
Jacob Parr <jacob.parr@corp.databricks.com>
2023-02-28 14:36:06 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from setuptools import find_packages
 
 setuptools.setup(
-    version="v3.0.42",
+    version="v3.0.43",
     name="dbacademy",
     author="Databricks, Inc",
     maintainer="Databricks Academy",
