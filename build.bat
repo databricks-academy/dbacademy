39aa8a98b7316c9e4634471f93f1564ad9e27fe6
Jacob Parr <jacob.parr@corp.databricks.com>
2023-03-02 22:34:17 -0600

    Updated docs

--- build.bat
+++ build.bat
@@ -5,7 +5,10 @@ C:\Users\jacob.parr\Anaconda3\envs\dbacademy\python.exe setup.py bdist_wheel
 rmdir /s/q .\build
 rmdir /s/q .\src\dbacademy.egg-info
 
-cd src
-pydoc -w dbacademy
-call move ./dbacademy.html ../docs/index.html
-cd ..
+RMDIR /q docs /Q /S
+MKDIR docs
+CD docs
+SET PYTHONPATH=..\src
+python -m pydoc -w ..\src\
+COPY dbacademy.html index.html
+
