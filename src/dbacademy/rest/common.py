afc7707ba6f7d1573227c1e2903d0111ebd547fa
SireInsectus <me@jacobparr.com>
2023-03-16 14:44:39 -0500

    Update common.py

--- src/dbacademy/rest/common.py
+++ src/dbacademy/rest/common.py
@@ -154,8 +154,8 @@ class ApiClient(ApiContainer):
         self.session = requests.Session()
         self.session.headers = {'Authorization': authorization_header, 'Content-Type': 'text/json'}
         
-        # noinspection HttpUrlsUsage
         self.http_adapter = HTTPAdapter(max_retries=retry)
+        # noinspection HttpUrlsUsage
         self.session.mount('http://', self.http_adapter)
         self.session.mount('https://', self.http_adapter)
 
