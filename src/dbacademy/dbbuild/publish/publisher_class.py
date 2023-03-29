40e2e51717168db8098797eaa09f6066b7d74b36
SireInsectus <me@jacobparr.com>
2023-03-28 19:43:08 -0500

    Update publisher_class.py

--- src/dbacademy/dbbuild/publish/publisher_class.py
+++ src/dbacademy/dbbuild/publish/publisher_class.py
@@ -336,7 +336,7 @@ class Publisher:
         :return: The HTML results that should be rendered with displayHTML() from the calling notebook
         """
         from dbacademy import dbgems
-        source_docs_path = f"{self.source_repo}/docs"
+        source_docs_path = f"/Workspace{self.source_repo}/docs"
 
         html = """<html><body style="font-size:16px">
                          <div>Contents written to...</div>\n"""
@@ -347,7 +347,7 @@ class Publisher:
             html += f"""<div><a href="https://labs.training.databricks.com/admin/courses/{self.build_name}/v{self.build_config.version}-PENDING/site/index.html" target="_blank">See /docs/{self.build_config.version}/index.html</a></div>"""
 
         if dest_repo:
-            target_docs_path = f"{self.target_dir}/docs/v{self.build_config.version}"
+            target_docs_path = f"Workspace{self.target_dir}/docs/v{self.build_config.version}"
             self.__copy_doc_files(source_docs_path, target_docs_path)
             html += f"""<div><a href="{dbgems.get_workspace_url()}#workspace{target_docs_path}/index.html" target="_blank">See /docs/{self.build_config.version}/index.html</a></div>"""
 
@@ -362,9 +362,10 @@ class Publisher:
         print(f"Target: {target_docs_path}")
 
         if os.path.exists(target_docs_path):
+            print(f"| Removing existing directory: {target_docs_path}")
             shutil.rmtree(target_docs_path)
 
-        shutil.copytree(src=f"/Workspace/{source_docs_path}",
+        shutil.copytree(src=source_docs_path,
                         dst=target_docs_path)
 
         print("-" * 80)
