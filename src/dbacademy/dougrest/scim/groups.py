da51f34b528a26a089377221177d29962ce8500a
SireInsectus <me@jacobparr.com>
2023-03-31 10:56:43 -0500

    Updated readability only

--- src/dbacademy/dougrest/scim/groups.py
+++ src/dbacademy/dougrest/scim/groups.py
@@ -45,7 +45,10 @@ class Groups(ApiContainer):
             raise Exception("Must provide group, group_id, or group_name")
         return self.databricks.api("PATCH", f"2.0/preview/scim/v2/Groups/{group_id}", _data={
             "schemas": ["urn:ietf:params:scim:api:messages:2.0:PatchOp"],
-            "Operations": [{"op": "remove", "path": f'entitlements[value eq "{entitlement}"]'}]
+            "Operations": [{
+                "op": "remove",
+                "path": f'entitlements[value eq "{entitlement}"]'
+            }]
         })
 
     def allow_cluster_create(self, value=True, group=None, group_id=None, group_name=None):
