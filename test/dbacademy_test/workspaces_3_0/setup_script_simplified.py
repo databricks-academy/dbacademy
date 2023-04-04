b50a62ecb5d350e39d4437bd4868bb26f85fd33e
Doug Bateman <doug@dougbateman.net>
2023-04-04 00:05:20 -0700

    Interim Commit: space setup_script_simplified.py

--- test/dbacademy_test/workspaces_3_0/setup_script_simplified.py
+++ test/dbacademy_test/workspaces_3_0/setup_script_simplified.py
@@ -1,6 +1,7 @@
 import os
-from typing import Optional, List
-from datetime import datetime
+import re
+from typing import List
+from time import sleep
 from dbacademy import common
 from dbacademy.dougrest import AccountsApi
 from dbacademy.workspaces_3_0.account_config_class import AccountConfig
@@ -86,8 +87,8 @@ workspace_setup.__accounts_api = AccountsApi(account_id=workspace_setup.account_
 
 workspace_config = workspace_setup.account_config.workspaces[0]
 
-workspace_api = workspace_setup.accounts_api.workspaces.create(workspace_name=workspace_config.name,
-                                                               deployment_name=workspace_config.name,
+workspace_api = workspace_setup.accounts_api.workspaces.create(workspace_name=workspace_config.workspace_name,
+                                                               deployment_name=workspace_config.workspace_name,
                                                                region=workspace_setup.account_config.region,
                                                                credentials_name=workspace_setup.account_config.workspace_config_template.credentials_name,
                                                                storage_configuration_name=workspace_setup.account_config.workspace_config_template.storage_configuration)
@@ -100,30 +101,196 @@ workspace = WorkspaceTrio(workspace_config, workspace_api, classroom)
 # Workspaces were all created synchronously, blocking here until we confirm that all workspaces are created.
 workspace.workspace_api.wait_until_ready()
 
-# Configuring the entitlements for the group "users"
-workspace_setup.update_entitlements(workspace)
+# Configure the entitlements for the group "users"
+entitlements = {
+    "allow-cluster-create": False,        # False to enforce policy
+    "allow-instance-pool-create": False,  # False to enforce policy
+    "databricks-sql-access": True,
+    "workspace-access": True,
+}
+users_group_id = workspace.client.scim.groups.get_by_name("users")["id"]
+for name, value in entitlements.items():
+    if value:
+        workspace.client.scim.groups.add_entitlement(users_group_id, name)
+    else:
+        workspace.client.scim.groups.remove_entitlement(users_group_id, name)
 
 # # Configuring users in each workspace.
 # workspace_setup.create_users(workspace)
 
-# Configure workspace-level groups
-workspace_setup.create_group(workspace)
+# Add instructor users as admins
+admin_users = ["class+000@databricks.com"]
+admins_group_id = workspace.client.scim.groups.get_by_name("admins")["id"]
+for user_name in admin_users:
+    user_id = workspace.get_by_username(username)["id"]
+    print("UserID", user_id)
+    workspace.client.scim.groups.add_member(admins_group_id, user_id)
 
-# Create the metastore for the workspace
-workspace_setup.create_metastore(workspace)
+# Create a new metastore
+metastore_id = workspace.workspace_api.api("POST", "2.1/unity-catalog/metastores", {
+    "name": workspace.workspace_config.name,
+    "storage_root": workspace_setup.account_config.uc_storage_config.storage_root,
+    "region": workspace_setup.account_config.uc_storage_config.region
+})["metastore_id"]
 
-# Enable workspace features
-workspace_setup.enable_features(workspace)
+# Configure the metastore settings
+# TODO: Add Instructors to a temporary instructors group for this workspace.
+workspace.workspace_api.api("PATCH", f"2.1/unity-catalog/metastores/{metastore_id}", {
+    "owner": workspace_setup.account_config.uc_storage_config.meta_store_owner,
+    "delta_sharing_scope": "INTERNAL_AND_EXTERNAL",
+    "delta_sharing_recipient_token_lifetime_in_seconds": 90 * 24 * 60 * 60,  # 90 days
+    "delta_sharing_organization_name": workspace.workspace_config.name
+})
+
+# Assign the metastore to the workspace
+workspace_id = workspace.workspace_api["workspace_id"]
+workspace.workspace_api.api("PUT", f"2.1/unity-catalog/workspaces/{workspace_id}/metastore", {
+    "metastore_id": metastore_id,
+    "default_catalog_name": "main"
+})
+
+# Grant all users permission to create resources in the metastore
+workspace.workspace_api.api("PATCH", f"2.1/unity-catalog/permissions/metastore/{metastore_id}", {
+    "changes": [{
+        "principal": "account users",
+        "add": ["CREATE CATALOG", "CREATE EXTERNAL LOCATION", "CREATE SHARE", "CREATE RECIPIENT", "CREATE PROVIDER"]
+    }]
+})
+
+# Create the storage credential for the metastore
+credentials_spec = {
+    "name": workspace.workspace_config.name,
+    "skip_validation": False,
+    "read_only": False,
+}
+if workspace_setup.account_config.uc_storage_config.aws_iam_role_arn is not None:
+    credentials_spec["aws_iam_role"] = {
+        "role_arn": workspace_setup.account_config.uc_storage_config.aws_iam_role_arn
+    }
+if workspace_setup.account_config.uc_storage_config.msa_access_connector_id is not None:
+    credentials_spec["azure_managed_identity"] = {
+        "access_connector_id": workspace_setup.account_config.uc_storage_config.msa_access_connector_id
+    }
+credentials = workspace.workspace_api.api("POST", "2.1/unity-catalog/storage-credentials", credentials_spec)
+storage_root_credential_id = credentials["id"]
+workspace.workspace_api.api("PATCH", f"2.1/unity-catalog/metastores/{metastore_id}", {
+    "storage_root_credential_id": storage_root_credential_id
+})
+
+# Enabling serverless SQL Warehouses.
+settings = workspace.workspace_api.api("GET", "2.0/sql/config/endpoints")
+settings["enable_serverless_compute"] = True
+workspace.workspace_api.api("PUT", "2.0/sql/config/endpoints", settings)
+
+# Configure workspace feature flags
+workspace.workspace_api.api("PATCH", "2.0/workspace-conf", {
+    "enable-X-Frame-Options": "false",  # Turn off iframe prevention
+    "intercomAdminConsent": "false",    # Turn off product welcome
+    "enableDbfsFileBrowser": "true",    # Enable DBFS UI
+    "enableWebTerminal": "true",        # Enable Web Terminal
+    "enableExportNotebook": "true"      # We will disable this in due time
+})
 
 # # Install courseware for all users in the workspace
 # workspace_setup.install_courseware(workspac)
 
+# Cloud specific settings for the Universal Workspace-Setup
+if ".cloud.databricks.com" in workspace.url:  # AWS
+    cloud_attributes = {
+        "node_type_id": "i3.xlarge",
+        "aws_attributes": {
+            "first_on_demand": 1,
+            "availability": "SPOT_WITH_FALLBACK",
+            "spot_bid_price_percent": 100
+        },
+    }
+elif ".gcp.databricks.com" in workspace.url:  # GCP
+    cloud_attributes = {
+        "node_type_id": "n1-highmem-4",
+        "gcp_attributes": {
+            "use_preemptible_executors": True,
+            "availability": "PREEMPTIBLE_WITH_FALLBACK_GCP",
+        },
+    }
+elif ".azuredatabricks.net" in workspace.url:  # Azure
+    cloud_attributes = {
+        "node_type_id": "Standard_DS3_v2",
+        "azure_attributes": {
+            "first_on_demand": 1,
+            "availability": "SPOT_WITH_FALLBACK_AZURE"
+        },
+    }
+else:
+    raise ValueError("Workspace is in unknown cloud.")
+
 # Run the Universal Workspace-Setup
-workspace_setup.run_workspace_setup(workspace)
+workspace_hostname = re.match("https://([^/]+)/api/", workspace.url)[1]
+job_spec = {
+    "name": "DBAcademy Workspace-Setup",
+    "timeout_seconds": 60 * 60 * 6,  # 6 hours
+    "max_concurrent_runs": 1,
+    "tasks": [{
+        "task_key": "Workspace-Setup",
+        "notebook_task": {
+            "notebook_path": "Workspace-Setup",
+            "base_parameters": {
+                "lab_id": f"Classroom #{workspace.workspace_config.workspace_number}",
+                "description": f"Classroom #{workspace.workspace_config.workspace_number}",
+                # "node_type_id": workspace.workspace_config.default_node_type_id,
+                # "spark_version": workspace.workspace_config.default_dbr,
+                "datasets": ",".join(workspace.workspace_config.dbc_urls),
+                "courses": ",".join(workspace.workspace_config.datasets),
+            },
+            "source": "GIT"
+        },
+        "job_cluster_key": "Workspace-Setup-Cluster",
+        "timeout_seconds": 0
+    }],
+    "job_clusters": [{
+        "job_cluster_key": "Workspace-Setup-Cluster",
+        "new_cluster": {
+            "spark_version": "11.3.x-cpu-ml-scala2.12",
+            "spark_conf": {
+                "spark.master": "local[*, 4]",
+                "spark.databricks.cluster.profile": "singleNode"
+            },
+            "custom_tags": {
+                "ResourceClass": "SingleNode",
+                "dbacademy.event_name": "Unknown",
+                "dbacademy.event_description": "Unknown",
+                "dbacademy.workspace": workspace_hostname
+            },
+            "spark_env_vars": {
+                "PYSPARK_PYTHON": "/databricks/python3/bin/python3"
+            },
+            "enable_elastic_disk": True,
+            "data_security_mode": "SINGLE_USER",
+            "runtime_engine": "STANDARD",
+            "num_workers": 0
+        }
+    }],
+    "git_source": {
+        "git_url": "https://github.com/databricks-academy/workspace-setup.git",
+        "git_provider": "gitHub",
+        "git_branch": "published"
+    },
+    "format": "MULTI_TASK"
+}
+job_spec["job_clusters"][0]["new_cluster"].update(cloud_attributes)
+job_id = workspace.jobs.create_multi_task_job(**job_spec)
+run_id = workspace.api("POST", "/2.1/jobs/run-now", {"job_id": job_id})["run_id"]
 
+# Wait for Universal-Workspace-Setup run completion
+while True:
+    response = workspace.api("GET", f"/2.1/jobs/runs/get?run_id={run_id}")
+    life_cycle_state = response.get("state").get("life_cycle_state")
+    if life_cycle_state not in ["PENDING", "RUNNING", "TERMINATING"]:
+        job_state = response.get("state", {})
+        job_result = job_state.get("result_state")
+        job_message = job_state.get("state_message", "Unknown")
+        break
+    sleep(60)  # seconds
+if job_result != "SUCCESS":
+    raise Exception(
+        f"""Expected the final state of Universal-Workspace-Setup to be "SUCCESS", found "{job_result}" for "{workspace.name}" | {job_message}""")
 
-for error in workspace_setup.errors:
-    print(error)
-    print("-" * 100)
-
-print(f"Completed update at {datetime.now()}")
