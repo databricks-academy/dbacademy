357a3073cdd4eae0f0899518d1d66dda266bb3ae
Jacob D. Parr <jacob.parr@databricks.com>
2023-06-05 15:13:53 -0500

    Publishing v3.0.75

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 rmdir /s/q .\dist
 
-C:\Users\jacob.parr\Anaconda3\envs\dbacademy\python.exe setup.py bdist_wheel
+C:\Users\JacobParr\Anaconda3\envs\dbacademy\python.exe setup.py bdist_wheel
 
 rmdir /s/q .\build
 rmdir /s/q .\src\dbacademy.egg-info
