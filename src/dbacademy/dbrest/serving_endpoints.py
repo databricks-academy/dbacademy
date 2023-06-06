7d67017d2e1bb951cd83b810dd30270262138164
Jacob D. Parr <jacob.parr@databricks.com>
2023-06-06 16:31:41 -0500

    Update serving_endpoints.py

--- src/dbacademy/dbrest/serving_endpoints.py
+++ src/dbacademy/dbrest/serving_endpoints.py
@@ -5,7 +5,16 @@ from dbacademy.rest.common import ApiContainer
 class ServingEndpointsClient(ApiContainer):
     def __init__(self, client: DBAcademyRestClient):
         self.client = client
+        self.base_url = f"{self.client.endpoint}/api/2.0/serving-endpoints"
 
     def list_endpoints(self) -> dict:
-        response = self.client.api("GET", f"{self.client.endpoint}/api/2.0/serving-endpoints")
+        response = self.client.api("GET", self.base_url)
         return response.get("endpoints", list())
+
+    def get_by_name(self, name):
+        self.client.api("GET", f"{self.base_url}/{name}")
+        return None
+
+    def delete_by_name(self, name):
+        self.client.api("DELETE", f"{self.base_url}/{name}")
+        return None
