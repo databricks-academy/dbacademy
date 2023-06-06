4ae8946a53c815f3c98549bf2fc61c590c11e236
Jacob D. Parr <jacob.parr@databricks.com>
2023-06-06 16:08:31 -0500

    Update workspace_cleaner_class.py

--- src/dbacademy/dbhelper/workspace_cleaner_class.py
+++ src/dbacademy/dbhelper/workspace_cleaner_class.py
@@ -330,13 +330,12 @@ class WorkspaceCleaner:
     def _cleanup_mlflow_endpoints(self, lesson_only: bool) -> bool:
         from dbacademy import dbgems
 
-        endpoints = []
         start = dbgems.clock_start()
 
         # Filter out the endpoints that pertain to this course and user
         unique_name = self._get_unique_name(lesson_only)
         # endpoints = self.__da.client.ml.mlflow_endpoints.list_endpoints()
-        endpoints = [] # self.__da.client.serving_endpoints.list_endpoints()
+        endpoints = self.__da.client.serving_endpoints.list_endpoints()
         print(f"| Found {len(endpoints)} endpoints.")
         for endpoint in endpoints:
             print("-"*80)
