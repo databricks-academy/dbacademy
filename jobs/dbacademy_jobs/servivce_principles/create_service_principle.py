61cc44d435842573b4c511d1f133015c677df0c8
Jacob D. Parr <jacob.parr@databricks.com>
2023-06-16 19:52:57 -0500

    Publishing v3.0.83

---
+++ jobs/dbacademy_jobs/servivce_principles/create_service_principle.py
@@ -0,0 +1,49 @@
+import json
+from dbacademy.dbrest import DBAcademyRestClient
+from dbacademy import common
+
+config_file = r"c:\users\JacobParr\.databrickscfg"
+
+# TODO file bug
+environments = ["dev-aws", "dev-msa"]  # , "dev-gcp"
+for environment in environments:
+    configs = common.load_databricks_cfg(config_file)
+    token = configs.get(environment).get("token")
+    endpoint = configs.get(environment).get("host")
+
+    client = DBAcademyRestClient(token=token, endpoint=endpoint)
+
+    admins = client.scim.groups.get_by_name("admins")
+    admins_id = admins.get("id")
+
+    sp_name = "Smoke Tester (Admin)"
+    sp = client.scim.service_principals.get_by_name(sp_name)
+    if sp is None:
+        client.scim.service_principals.create(display_name=sp_name,
+                                              group_ids=[admins_id],
+                                              entitlements=[])
+        # self, display_name: str, group_ids: List = None, entitlements: List = None
+
+    sp = client.scim.service_principals.get_by_name(sp_name)
+    sp_id = sp.get("id")
+    application_id = sp.get("applicationId")
+
+    print("-"*80)
+    print(f"Endpoint: {client.endpoint}")
+    print(sp.get("displayName"))
+
+    tokens = client.token_management.list()
+    sp_token = None
+    for token in tokens:
+        owner_id = token.get("owner_id")
+        created_by = token.get("created_by_username")
+        comment = token.get("comment")
+        if application_id == created_by:
+            print("Found token for SP.")
+            sp_token = token
+            # client.token_management.delete_by_id(token.get("token_id"))
+            # print(json.dumps(token, indent=4))
+
+    if sp_token is None:
+        client.token_management.create_on_behalf_of_service_principal(application_id=application_id, comment="Test Automation", lifetime_seconds=None)
+    pass
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from setuptools import find_packages
 
 setuptools.setup(
-    version="v3.0.82",
+    version="v3.0.83",
     name="dbacademy",
     author="Databricks, Inc",
     maintainer="Databricks Academy",
