f920f4a790637766fcf07c35256454fc234cf73a
Jacob D. Parr <jacob.parr@databricks.com>
2023-07-25 14:01:42 -0500

    Minor updates

--- jobs/dbacademy_jobs/workspaces_3_0/universal_workspace_setup_demo.py
+++ jobs/dbacademy_jobs/workspaces_3_0/universal_workspace_setup_demo.py
@@ -1,4 +1,7 @@
 import os
+from dbacademy.common import Cloud
+from dbacademy.dbgems import stable_hash
+from dbacademy.dbhelper import WorkspaceHelper
 from dbacademy.dbrest import DBAcademyRestClient
 
 
@@ -7,7 +10,6 @@ def to_workspace_name(_event_id: int) -> str:
     The name generated here is done in accordance with a
     standard used in CloudLabs, AirTable and Backup workspaces.
     """
-    from dbacademy.dbgems import stable_hash
 
     name = f"classroom-{_event_id:03d}"
     salt = "Databricks Lakehouse"   # A string we use to generate the hash with
@@ -15,12 +17,15 @@ def to_workspace_name(_event_id: int) -> str:
     return f"{name}-{hashcode}".lower()
 
 
-def create_job(_cloud: str, _event_id: int, _workspace_name: str, _context: str) -> str:
+def create_job(_cloud: Cloud, _event_id: int, _workspace_name: str, _context: str) -> str:
     """
-    Creates the job in the target's Databricks workspace using Databricks' REST API.
+    Creates the job in the target's Databricks workspace using Databricks' REST API. Aside from parameters used
+    by the various API calls, some of these parameters also results in custom tracks for tracking and reporting purposes.
     :param _cloud: Identifies the cloud of the target's Databricks workspace; one of AWS, MSA or GCP.
     :param _event_id: Optional 3+ digit code for tracking, ideally an integer.
-    :param _workspace_name: E2 accounts have a name associated with each workspace which in turn is part of the workspaces' URL.
+    :param _workspace_name: E2 accounts have a name associated with each workspace which in turn is part of the
+    workspaces' URL. In the case of the "training" E2 account, and the workspace name "classroom-700-rw69a",
+    we wil get the URL https://training-classroom-700-rw69a.cloud.databricks.com
     :param _context: The assets' context; e.g. AirTable, Backup, CloudLabs' tenant, Vocareum's tenant, etc.
     :return: The job's ID.
     """
@@ -47,17 +52,17 @@ def create_job(_cloud: str, _event_id: int, _workspace_name: str, _context: str)
     # The job's JSON file includes config for AWS, MSA and GCP. The following section makes
     # the required changes (e.g. dropping the prefixes) to conform the cloud-specific requirements.
 
-    if _cloud == "aws":
+    if _cloud == Cloud.AWS:
         # Enable AWS parameters
         config_text = config_text.replace("aws:node_type_id", "node_type_id")
         config_text = config_text.replace("aws:aws_attributes", "aws_attributes")
 
-    elif _cloud == "msa":
+    elif _cloud == Cloud.MSA:
         # Enable MSA parameters
         config_text = config_text.replace("msa:node_type_id", "node_type_id")
         config_text = config_text.replace("msa:aws_attributes", "aws_attributes")
 
-    elif _cloud == "gcp":
+    elif _cloud == Cloud.GCP:
         # Enable GCP parameters
         config_text = config_text.replace("gcp:node_type_id", "node_type_id")
         config_text = config_text.replace("gcp:aws_attributes", "aws_attributes")
@@ -67,25 +72,25 @@ def create_job(_cloud: str, _event_id: int, _workspace_name: str, _context: str)
     return client.jobs.create_from_dict(config)
 
 
-# The event_id and workspace_name convention come from other deployments
-event_id = 700  # Workspace 700 is Jacob's test workspace
-workspace_name = to_workspace_name(event_id)
+event_id = 700                                                            # Workspace 700 is Jacob's test workspace
+workspace_name = to_workspace_name(event_id)                              # This convention comes from other in-house deployments
 
-env_code = "PROSVC"  # Convention used by Labs Platform
-username = os.environ.get(f"WORKSPACE_SETUP_{env_code}_USERNAME")
-password = os.environ.get(f"WORKSPACE_SETUP_{env_code}_PASSWORD")
-endpoint = f"https://training-{workspace_name}.cloud.databricks.com"
+env_code = "PROSVC"                                                       # Convention used by Labs Platform
+username = os.environ.get(f"WORKSPACE_SETUP_{env_code}_USERNAME")         # The username employed by the REST client
+password = os.environ.get(f"WORKSPACE_SETUP_{env_code}_PASSWORD")         # The password employed by the REST client
+endpoint = f"https://training-{workspace_name}.cloud.databricks.com"      # Construct the endpoint's URL
 
 client = DBAcademyRestClient(endpoint=endpoint, user=username, password=password)
 assert len(client.workspace.ls("/")) > 0, f"Expected at least one file."  # Testing connection
 
-client.jobs.delete_by_name("DBAcademy Workspace-Setup", False)  # Hard coded into the corresponding JSON file.)
-job_id = create_job("aws", event_id, workspace_name, "Demo")  # Context descriptor; AirTable, Backup, CloudLabs, Vocareum
-run_id = client.jobs.run_now(job_id).get("run_id")
-response = client.runs.wait_for(run_id)
+job_name = WorkspaceHelper.WORKSPACE_SETUP_JOB_NAME                       # The job's name allowing for configuration by convention
+client.jobs.delete_by_name(job_name, False)                               # Hard coded into the corresponding JSON file.)
+job_id = create_job(Cloud.AWS, event_id, workspace_name, "Demo")              # Create the job using the DBRest client
+run_id = client.jobs.run_now(job_id).get("run_id")                        # Run the job using the DBRest client
+response = client.runs.wait_for(run_id)                                   # Blocks until the job has completed
 
-result_state = response.get("state").get("result_state")
+result_state = response.get("state").get("result_state")                  # Validate that results state.
 assert result_state == "SUCCESS", f"""Expected the result_state to be "SUCCESS", found "{result_state}"."""
 
-life_cycle_state = response.get("state").get("life_cycle_state")
+life_cycle_state = response.get("state").get("life_cycle_state")          # Validate the life cycle's state.
 assert life_cycle_state == "TERMINATED", f"""Expected the life_cycle_state to be "TERMINATED", found "{life_cycle_state}"."""
