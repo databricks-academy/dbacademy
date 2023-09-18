b02385fb1c66c42380b91d246e593a6516481dc0
Jacob D. Parr <jacob.parr@databricks.com>
2023-09-18 16:23:16 -0500

    Update __init__.py

--- src/dbacademy/dbrest/accounts/__init__.py
+++ src/dbacademy/dbrest/accounts/__init__.py
@@ -6,10 +6,11 @@ from dbacademy.clients.rest.common import ApiContainer
 class AccountsClient(ApiContainer):
     from dbacademy.clients.rest.common import ApiClient
 
-    def __init__(self, *, endpoint: str, account_id: str, username: str, password: str):
+    def __init__(self, *, account_id: str, username: str, password: str):
         from dbacademy.dbrest.client import DBAcademyRestClient
 
-        self.client = DBAcademyRestClient(endpoint=endpoint, user=username, password=password)
+        self.endpoint = "https://accounts.cloud.databricks.com"
+        self.client = DBAcademyRestClient(endpoint=self.endpoint, user=username, password=password)
         self.account_id = account_id
 
         from dbacademy.dbrest.accounts.scim import AccountScimClient
