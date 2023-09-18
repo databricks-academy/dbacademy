2d0d36493218691e32a3e743259819e3243558c1
Jacob D. Parr <jacob.parr@databricks.com>
2023-09-18 15:45:20 -0500

    Update workspaces.py

--- src/dbacademy/dbrest/accounts/workspaces.py
+++ src/dbacademy/dbrest/accounts/workspaces.py
@@ -8,8 +8,6 @@ class WorkspacesClient(ApiContainer):
     from dbacademy.clients.rest.common import ApiClient
 
     def __init__(self, client: ApiClient, account_id: str):
-        super().__init__(client)
-
         self.account_id = account_id
         self.client = client
         self.base_url = f"{self.client.url}/api/2.0/accounts/{self.account_id}/workspace"
