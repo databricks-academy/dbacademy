14c7adc5fb8885ae1de667d125d2e5a9fe53d1d4
Jacob D. Parr <jacob.parr@databricks.com>
2023-09-20 23:03:54 -0500

    Update secrets.py

--- src/dbacademy/dbrest/secrets.py
+++ src/dbacademy/dbrest/secrets.py
@@ -14,8 +14,8 @@ class SecretsClient(ApiContainer):
         self.base_url = f"{self.client.endpoint}/api/2.0/secrets"
 
     def scopes_list(self) -> List[Dict[str, Any]]:
-        scopes = self.client.api("GET", f"{self.base_url}/scopes/list")
-        return scopes or list()
+        response = self.client.api("GET", f"{self.base_url}/scopes/list")
+        return response.get("scopes", list())
 
     def scopes_create(self, scope: str, initial_manage_principal: str = None, scope_backend_type: SCOPE_BACKEND_TYPE = "DATABRICKS") -> None:
         return self.client.api("POST", f"{self.base_url}/scopes/create", scope=scope, initial_manage_principal=initial_manage_principal, scope_backend_type=scope_backend_type)
@@ -24,5 +24,5 @@ class SecretsClient(ApiContainer):
         return self.client.api("POST", f"{self.base_url}/scopes/delete", scope=scope)
 
     def secrets_list(self, scope: str) -> List[Dict[str, Any]]:
-        secrets = self.client.api("GET", f"{self.base_url}/secrets/list", scope=scope)
-        return secrets or list()
+        response = self.client.api("GET", f"{self.base_url}/secrets/list", scope=scope)
+        return response.get("secrets", list())
