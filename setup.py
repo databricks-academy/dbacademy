2f6558bd21358c626c3eba231ef55f448555f62c
Jacob D. Parr <jacob.parr@databricks.com>
2023-10-23 11:11:34 -0500

    Publishing v5.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from setuptools import find_packages
 
 setuptools.setup(
-    version="v5.0.3",
+    version="v5.0.4",
     name="dbacademy",
     author="Databricks, Inc",
     maintainer="Databricks Academy",
