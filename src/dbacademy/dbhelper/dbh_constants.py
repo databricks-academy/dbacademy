58205f2f429a450e15f904e946460738d59f5578
Jacob D. Parr <jacob.parr@databricks.com>
2023-11-16 15:56:19 -0600

    Update dbh_constants.py

--- src/dbacademy/dbhelper/dbh_constants.py
+++ src/dbacademy/dbhelper/dbh_constants.py
@@ -1,61 +1,140 @@
 __all__ = ["WORKSPACE_HELPER", "CLUSTERS_HELPER", "DBACADEMY_HELPER", "WAREHOUSE_HELPER"]
 
+from typing import List
 
+
+# noinspection PyPep8Naming
 class WorkspaceHelperConstants:
 
-    def __init__(self):
-        # TODO convert to properties
-        self.WORKSPACE_SETUP_JOB_NAME = "DBAcademy Workspace-Setup"
-        self.BOOTSTRAP_JOB_NAME = "DBAcademy Workspace-Setup (Bootstrap)"
+    @property
+    def WORKSPACE_SETUP_JOB_NAME(self) -> str:
+        return "DBAcademy Workspace-Setup"
 
-        self.PARAM_EVENT_ID = "event_id"
-        self.PARAM_EVENT_DESCRIPTION = "event_description"
-        self.PARAM_POOLS_NODE_TYPE_ID = "pools_node_type_id"
-        self.PARAM_DEFAULT_SPARK_VERSION = "default_spark_version"
-        self.PARAM_DATASETS = "datasets"
-        self.PARAM_COURSES = "courses"
-        self.PARAM_SOURCE = "source"
-        self.PARAM_ORG_ID = "org_id"
-        self.PARAM_WORKSPACE_NAME = "workspace_name"
+    @property
+    def BOOTSTRAP_JOB_NAME(self) -> str:
+        return "DBAcademy Workspace-Setup (Bootstrap)"
+
+    @property
+    def PARAM_EVENT_ID(self) -> str:
+        return "event_id"
+
+    @property
+    def PARAM_EVENT_DESCRIPTION(self) -> str:
+        return "event_description"
+
+    @property
+    def PARAM_POOLS_NODE_TYPE_ID(self) -> str:
+        return "pools_node_type_id"
+
+    @property
+    def PARAM_DEFAULT_SPARK_VERSION(self) -> str:
+        return "default_spark_version"
+
+    @property
+    def PARAM_DATASETS(self) -> str:
+        return "datasets"
+
+    @property
+    def PARAM_COURSES(self) -> str:
+        return "courses"
+
+    @property
+    def PARAM_SOURCE(self) -> str:
+        return "source"
+
+    @property
+    def PARAM_ORG_ID(self) -> str:
+        return "org_id"
+
+    @property
+    def PARAM_WORKSPACE_NAME(self) -> str:
+        return "workspace_name"
 
 
+# noinspection PyPep8Naming
 class ClustersHelperConstants:
-    def __init__(self):
-        # TODO convert to properties
-        self.POLICY_ALL_PURPOSE = "DBAcademy"
-        self.POLICY_JOBS_ONLY = "DBAcademy Jobs"
-        self.POLICY_DLT_ONLY = "DBAcademy DLT"
-        self.POLICIES = [self.POLICY_ALL_PURPOSE, self.POLICY_JOBS_ONLY, self.POLICY_DLT_ONLY]
 
-        self.POOL_DEFAULT_NAME = "DBAcademy"
-        self.POOLS = [self.POOL_DEFAULT_NAME]
+    @property
+    def POLICY_ALL_PURPOSE(self) -> str:
+        return "DBAcademy"
+
+    @property
+    def POLICY_JOBS_ONLY(self) -> str:
+        return "DBAcademy Jobs"
+
+    @property
+    def POLICY_DLT_ONLY(self) -> str:
+        return "DBAcademy DLT"
+
+    @property
+    def POLICIES(self) -> List[str]:
+        return [self.POLICY_ALL_PURPOSE,
+                self.POLICY_JOBS_ONLY,
+                self.POLICY_DLT_ONLY]
+
+    @property
+    def POOL_DEFAULT_NAME(self) -> str:
+        return "DBAcademy"
+
+    @property
+    def POOLS(self) -> List[str]:
+        return [self.POOL_DEFAULT_NAME]
 
 
+# noinspection PyPep8Naming
 class DBAcademyHelperConstants:
 
-    def __init__(self):
-        # TODO convert to properties
-        self.SCHEMA_DEFAULT = "default"
-        self.SCHEMA_INFORMATION = "information_schema"
+    @property
+    def SCHEMA_DEFAULT(self) -> str:
+        return "default"
 
-        self.SPARK_CONF_SMOKE_TEST = "dbacademy.smoke-test"
-        self.SPARK_CONF_PATHS_DATASETS = "dbacademy.paths.datasets"
-        self.SPARK_CONF_PATHS_USERS = "dbacademy.paths.users"
-        self.SPARK_CONF_DATA_SOURCE_URI = "dbacademy.data-source-uri"
-        self.SPARK_CONF_PROTECTED_EXECUTION = "dbacademy.protected-execution"
-        self.SPARK_CONF_CLUSTER_TAG_SPARK_VERSION = "spark.databricks.clusterUsageTags.sparkVersion"
+    @property
+    def SCHEMA_INFORMATION(self) -> str:
+        return "information_schema"
 
-        self.CATALOG_SPARK_DEFAULT = "spark_catalog"
-        self.CATALOG_UC_DEFAULT = "hive_metastore"
+    @property
+    def SPARK_CONF_SMOKE_TEST(self) -> str:
+        return "dbacademy.smoke-test"
 
-        self.TROUBLESHOOT_ERROR_TEMPLATE = """{error} Please see the "Troubleshooting | {section}" section of the "Version Info" notebook for more information."""
+    @property
+    def SPARK_CONF_PATHS_DATASETS(self) -> str:
+        return "dbacademy.paths.datasets"
+
+    @property
+    def SPARK_CONF_PATHS_USERS(self) -> str:
+        return "dbacademy.paths.users"
+
+    @property
+    def SPARK_CONF_DATA_SOURCE_URI(self) -> str:
+        return "dbacademy.data-source-uri"
+
+    @property
+    def SPARK_CONF_PROTECTED_EXECUTION(self) -> str:
+        return "dbacademy.protected-execution"
+
+    @property
+    def SPARK_CONF_CLUSTER_TAG_SPARK_VERSION(self) -> str:
+        return "spark.databricks.clusterUsageTags.sparkVersion"
+
+    @property
+    def CATALOG_SPARK_DEFAULT(self) -> str:
+        return "spark_catalog"
+
+    @property
+    def CATALOG_UC_DEFAULT(self) -> str:
+        return "hive_metastore"
+
+    @property
+    def TROUBLESHOOT_ERROR_TEMPLATE(self) -> str:
+        return """{error} Please see the "Troubleshooting | {section}" section of the "Version Info" notebook for more information."""
 
 
+# noinspection PyPep8Naming
 class WarehouseHelperConstants:
 
-    def __init__(self):
-        # TODO convert to properties
-        self.WAREHOUSES_DEFAULT_NAME = "DBAcademy Warehouse"
+    @property
+    def WAREHOUSES_DEFAULT_NAME(self) -> str:
+        return "DBAcademy Warehouse"
 
 
 WORKSPACE_HELPER = WorkspaceHelperConstants()
