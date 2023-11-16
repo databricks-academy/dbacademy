dc22fe9994560a375a2179b02e3870c4040b6b2f
Jacob D. Parr <jacob.parr@databricks.com>
2023-11-16 14:54:19 -0600

    Update feature_store.py

--- src/dbacademy/clients/databricks/ml/feature_store.py
+++ src/dbacademy/clients/databricks/ml/feature_store.py
@@ -10,15 +10,15 @@ class FeatureStoreClient(ApiContainer):
         self.base_uri = f"{self.client.endpoint}/api/2.0/feature-store"
 
     def search_tables(self) -> List[Dict[str, Any]]:
-        import sys
+        maxsize = 200
         results = []
 
-        response = self.client.api("GET", f"{self.base_uri}/feature-tables/search?max_results={100}")
+        response = self.client.api("GET", f"{self.base_uri}/feature-tables/search?max_results={maxsize}")
         results.extend(response.get("feature_tables", []))
 
         while "next_page_token" in response:
             next_page_token = response["next_page_token"]
-            response = self.client.api("GET", f"{self.base_uri}/feature-tables/search?max_results={sys.maxsize}&page_token={next_page_token}")
+            response = self.client.api("GET", f"{self.base_uri}/feature-tables/search?max_results={maxsize}&page_token={next_page_token}")
             results.extend(response.get("feature_tables", []))
 
         return results
