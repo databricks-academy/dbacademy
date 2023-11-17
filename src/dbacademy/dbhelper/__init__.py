9d7dd61c6f32f890ac93bf48caf7dc954159300c
Jacob D. Parr <jacob.parr@databricks.com>
2023-11-17 16:01:21 -0600

    Update __init__.py

--- src/dbacademy/dbhelper/__init__.py
+++ src/dbacademy/dbhelper/__init__.py
@@ -1,10 +1,10 @@
 """
 This module contains utilities designed to from within a Databricks Notebook, for which the primary entry point to this module is the DBAcademyHelper (DA) object.
 """
-# from dbacademy.dbhelper.dbacademy_helper import DBAcademyHelper
-# from dbacademy.dbhelper.lesson_config import LessonConfig
-# from dbacademy.dbhelper.course_config import CourseConfig
-# from dbacademy.dbhelper.paths import Paths
+from dbacademy.dbhelper.dbacademy_helper import DBAcademyHelper
+from dbacademy.dbhelper.lesson_config import LessonConfig
+from dbacademy.dbhelper.course_config import CourseConfig
+from dbacademy.dbhelper.paths import Paths
 # from dbacademy.dbhelper.validations import ValidationSuite
 # from dbacademy.dbhelper.supporting.workspace_helper import WorkspaceHelper
 # from dbacademy.dbhelper.supporting.clusters_helper import ClustersHelper
