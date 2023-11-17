c79b978600fce5e7cb7684134c74cfbaeb057e22
Jacob D. Parr <jacob.parr@databricks.com>
2023-11-16 22:15:30 -0600

    Update translator_class.py

--- src/dbacademy/dbbuild/publish/translator_class.py
+++ src/dbacademy/dbbuild/publish/translator_class.py
@@ -112,7 +112,8 @@ class Translator:
 
                     if NotebookDef.is_not_titled(cm=cm, command=command):
                         pos = line_zero.find("--i18n-")
-                        line_zero = line_zero[:pos].strip() if pos >= 0 else line_zero
+                        line_zero = line_zero[:pos].strip() if pos >= 0 else line_zero.strip()
+                        assert line_zero == f"{cm} MAGIC %md", f"""Cmd #{i+1} | Line zero contains more than just "%md"."""
                         lines.insert(0, line_zero)
 
                     # Add the title back in
