be7f7f19fd6055d3eb814bbc4f21b2ca8b22045e
Jacob D. Parr <jacob.parr@databricks.com>
2023-11-22 14:08:01 -0600

    More type fixes

--- src/dbacademy/clients/darest/permissions/__init__.py
+++ src/dbacademy/clients/darest/permissions/__init__.py
@@ -1,37 +1,67 @@
 __all__ = ["Permissions"]
 
-from dbacademy.clients.rest.common import ApiClient, ApiContainer
+from dbacademy.clients.rest.common import ApiContainer
+from dbacademy.clients.darest import DBAcademyRestClient
+from dbacademy.clients.darest.permissions.clusters import Clusters
+from dbacademy.clients.darest.permissions.directories import Directories
+from dbacademy.clients.darest.permissions.jobs import Jobs
+from dbacademy.clients.darest.permissions.pools import Pools
+from dbacademy.clients.darest.permissions.sql import Sql
+from dbacademy.clients.darest.permissions.cluster_policies import ClusterPolicies
+from dbacademy.clients.darest.permissions.warehouses import Warehouses
+from dbacademy.clients.darest.permissions.authorization_tokens import Tokens
+
+
+class Authorization:
+    def __init__(self, client: DBAcademyRestClient):
+        self.__client = client
+
+    @property
+    def client(self) -> DBAcademyRestClient:
+        return self.__client
+
+    @property
+    def tokens(self) -> Tokens:
+        return Tokens(self.client)
 
 
 class Permissions(ApiContainer):
 
-    def __init__(self, client: ApiClient):
-        self.client = client
+    def __init__(self, client: DBAcademyRestClient):
+        self.__client = client
 
-        from dbacademy.clients.darest.permissions.clusters import Clusters
-        self.clusters = Clusters(client)
+    @property
+    def client(self) -> DBAcademyRestClient:
+        return self.__client
 
-        from dbacademy.clients.darest.permissions.directories import Directories
-        self.directories = Directories(client)
+    @property
+    def clusters(self) -> clusters:
+        return Clusters(self.client)
 
-        from dbacademy.clients.darest.permissions.jobs import Jobs
-        self.jobs = Jobs(client)
+    @property
+    def directories(self) -> directories:
+        return Directories(self.client)
 
-        from dbacademy.clients.darest.permissions.pools import Pools
-        self.pools = Pools(client)
+    @property
+    def jobs(self) -> jobs:
+        return Jobs(self.client)
 
-        from dbacademy.clients.darest.permissions.sql import Sql
-        self.sql = Sql(client)
+    @property
+    def pools(self) -> pools:
+        return Pools(self.client)
 
-        from dbacademy.clients.darest.permissions.cluster_policies import ClusterPolicies
-        self.cluster_policies = ClusterPolicies(client)
+    @property
+    def sql(self) -> sql:
+        return Sql(self.client)
 
-        from dbacademy.clients.darest.permissions.warehouses import Warehouses
-        self.warehouses = Warehouses(client)
+    @property
+    def cluster_policies(self) -> cluster_policies:
+        return ClusterPolicies(self.client)
 
-        class Authorization:
-            def __init__(self):
-                from dbacademy.clients.darest.permissions.authorization_tokens import Tokens
-                self.tokens = Tokens(client)
+    @property
+    def warehouses(self) -> warehouses:
+        return Warehouses(self.client)
 
-        self.authorizations = Authorization()
+    @property
+    def authorizations(self) -> Authorization:
+        return Authorization(self.client)
