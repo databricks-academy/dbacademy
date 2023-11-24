8c970c1b29c01d135cc33da7daa506f009b33481
Jacob D. Parr <jacob.parr@databricks.com>
2023-11-24 15:26:05 -0600

    Update build_config.py

--- src/dbacademy/dbbuild/build_config.py
+++ src/dbacademy/dbbuild/build_config.py
@@ -751,7 +751,7 @@ def __load_from_dict(*, config: Dict[str, Any], name: str, value: ParameterType,
         value = config[name]
         del config[name]  # Delete the entry for later validation.
         print(f"""Using value from config: {name} = {value}""")
-        return_value = validate(parameter_name_override=name, value=value).as_type(expected_type)
+        return_value = validate(value=value).args(parameter_name=name).as_type(expected_type)
         print(f"""   {return_value}""")
         return return_value
 
