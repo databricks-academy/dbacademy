f0211ab033fdc7a7a242578ed04170c55871e29d
Jacob D. Parr <jacob.parr@databricks.com>
2023-11-24 21:35:03 -0600

    Update change_log.py

--- src/dbacademy/dbbuild/change_log.py
+++ src/dbacademy/dbbuild/change_log.py
@@ -1,6 +1,6 @@
 __all__ = ["ChangeLog"]
 
-from typing import Optional
+from typing import List, Optional
 
 
 class ChangeLog:
@@ -9,47 +9,15 @@ class ChangeLog:
     CHANGE_LOG_VERSION = "### Version "
 
     def __init__(self, *, source_repo: str, readme_file_name: str, target_version: Optional[str]):
+        import os
 
         self.__source_repo = source_repo
         self.__target_version = target_version
         self.__readme_file_name = readme_file_name
 
-        self.entries = []
-        self.version: Optional[str] = None
-        self.date: Optional[str] = None
-
-        self.__load()
-
-    @property
-    def readme_file_name(self):
-        return self.__readme_file_name
-
-    @property
-    def source_repo(self):
-        return self.__source_repo
-
-    @property
-    def target_version(self):
-        return self.__target_version
-
-    def __str__(self):
-        string = f"Change Log: v{self.version} ({self.date})"
-        for entry in self.entries:
-            string += f"\n  {entry}"
-        return string
-
-    def print(self):
-        print(self)
-
-    def validate(self, expected_version: str, date: Optional[str]):
-        from datetime import datetime
-        assert self.version == expected_version, f"The change log entry's version is not \"{expected_version}\", found \"{self.version}\"."
-
-        date = date or datetime.today().strftime("%-m-%-d-%Y")
-        assert self.date == f"{date}", f"The change log entry's date is not \"{date}\", found \"{self.date}\"."
-
-    def __load(self):
-        import os
+        self.__entries = []
+        self.__version: Optional[str] = None
+        self.__date: Optional[str] = None
 
         readme_path = f"/Workspace{self.source_repo}/{self.readme_file_name}"
         assert os.path.exists(readme_path), f"The README file was not found at {readme_path}"
@@ -79,7 +47,7 @@ class ChangeLog:
                 assert parts[0] == "###", f"Part 1 of the change long entry is not \"###\", found \"{parts[0]}\""
                 assert parts[1] == "Version", f"Part 2 of the change long entry is not \"Version\", found \"{parts[1]}\""
 
-                self.version = parts[2]
+                self.__version = parts[2]
 
                 v_parts = self.version.split(".")
                 assert len(v_parts) == 3, f"The change long entry's version field is not of the form \"vN.N.N\" where \"N\" is an integral value, found {len(v_parts)} parts: \"{self.version}\"."
@@ -94,10 +62,10 @@ class ChangeLog:
                 else:
                     continue
 
-                self.date = parts[3]
+                self.__date = parts[3]
                 assert self.date.startswith("(") and self.date.endswith(")"), f"Expected the change log entry's date field to be of the form \"(M-D-YYYY)\" or \"(TBD)\", found \"{self.date}\" for version \"{self.version}\"."
 
-                self.date = self.date[1:-1]
+                self.__date = self.date[1:-1]
                 if self.date != "TBD":
                     d_parts = self.date.split("-")
                     assert len(d_parts) == 3, f"The change long entry's date field is not of the form \"(M-D-YYYY)\", found {self.date}\" for version \"{self.version}\"."
@@ -109,6 +77,46 @@ class ChangeLog:
                 self.entries.append(line)
 
             elif version_index and i > version_index and line.startswith("#"):
-                return self
+                return  # Bail out
 
         assert len(self.entries) > 0, f"The Change Log section was not found in {readme_path}"
+
+    @property
+    def entries(self) -> List[str]:
+        return self.__entries
+
+    @property
+    def version(self) -> Optional[str]:
+        return self.__version
+
+    @property
+    def date(self) -> Optional[str]:
+        return self.__date
+
+    @property
+    def readme_file_name(self) -> str:
+        return self.__readme_file_name
+
+    @property
+    def source_repo(self) -> str:
+        return self.__source_repo
+
+    @property
+    def target_version(self) -> str:
+        return self.__target_version
+
+    def __str__(self):
+        string = f"Change Log: v{self.version} ({self.date})"
+        for entry in self.entries:
+            string += f"\n  {entry}"
+        return string
+
+    def print(self) -> None:
+        print(self)
+
+    def validate(self, expected_version: str, date: Optional[str]) -> None:
+        from datetime import datetime
+        assert self.version == expected_version, f"The change log entry's version is not \"{expected_version}\", found \"{self.version}\"."
+
+        date = date or datetime.today().strftime("%-m-%-d-%Y")
+        assert self.date == f"{date}", f"The change log entry's date is not \"{date}\", found \"{self.date}\"."
