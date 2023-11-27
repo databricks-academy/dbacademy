0b40f581dd35b26bca620666b0a051b3527141d7
Jacob D. Parr <jacob.parr@databricks.com>
2023-11-27 04:37:10 -0600

    Update __init__.py

--- src/dbacademy/clients/dbrest/jobs_api/__init__.py
+++ src/dbacademy/clients/dbrest/jobs_api/__init__.py
@@ -21,6 +21,11 @@ class JobsApi(ApiContainer):
     def create_from_dict(self, params: Dict[str, Any]) -> str:
         params = validate(params=params).required.dict(str)
 
+        import json
+        print("="*80)
+        print(json.dumps(params, indent=4))
+        print("="*80)
+
         response = self.__client.api("POST", f"{self.base_uri}/create", params)
         return response.get("job_id")
 
